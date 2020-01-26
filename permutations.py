cb6d774a3440ff4b45af37924702eb7eec06ba30
Karl Fredrickson <karlfredrickson@gmail.com>
2020-01-26 01:06:30 -0800

    First commit

---
+++ permutations.py
@@ -0,0 +1,65 @@
+#f and g have form: f[0] is frozenset of tuples
+#f[1] is dict
+#Tuples and dict do not have any elements mapped to themselves
+def compose(f,g):
+    tupleList = []
+    newDict = {}
+    for x in g[1]:
+        img = g[1][x]
+        if img in f[1]:
+            img = f[1][img]
+        if x != img:
+            tupleList.append((x,img))
+            newDict[x] = img
+    for x in f[1]:
+        if x not in g[1]:
+            tupleList.append((x,f[1][x]))
+            newDict[x] = f[1][x]
+    return [frozenset(tupleList),newDict]
+
+#List of pairs of form:
+#f[0] is frozenset of tuples
+#f[1] is dict
+#Tuples and dict do not have any elements mapped to themselves
+def generateGroup(gens):
+    currentElementSet = set()
+    gpElements = []
+    for x in gens:
+        currentElementSet.add(x[0])
+        gpElements.append(x)
+    compStack = []
+    for x in gens:
+        for y in gens:
+            compStack.append((x,y))
+    while len(compStack) > 0:
+        x = compStack.pop()
+        p = compose(x[0],x[1])
+        if p[0] in currentElementSet:
+            pass
+        else:
+            gpElements.append(p)
+            currentElementSet.add(p[0])
+            for z in gpElements:
+                compStack.append((z,p))
+            for z in gpElements:
+                compStack.append((p,z))
+    return gpElements
+        
+def createPairs(gens):
+    pairList = []
+    for x in gens:
+        currentTuples = []
+        for y in x:
+            if x[y]==y:
+                del x[y]
+            else:
+                currentTuples.append((y,x[y]))
+        pairList.append([frozenset(currentTuples),x])
+    return pairList
+
+
+x = createPairs([{1:2,2:1},{1:2,2:3,3:1}])
+y = generateGroup(x)
+
+            
+        
